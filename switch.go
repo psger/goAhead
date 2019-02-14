1cacb51c7d0be3c954cf1be3913a2613bf4562d7
passenger <passengerlcw@gmail.com>
2019-02-14 16:22:12 +0800

    exercise for go

---
+++ switch.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 2
+	fmt.Print("write", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It`s the weekday")
+	default:
+		fmt.Println("It`s the workday")
+	}
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It`s before noon")
+	default:
+		fmt.Println("It`s after noon")
+	}
+
+	whatAmI := func(i interface{}) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I am a bool")
+		case int:
+			fmt.Println("I am a int")
+		default:
+			fmt.Println("Do not know type", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hello")
+}
---
+++ variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int // 初始值为0
+	fmt.Println(e)
+
+	f := "short"
+	fmt.Println(f)
+}
